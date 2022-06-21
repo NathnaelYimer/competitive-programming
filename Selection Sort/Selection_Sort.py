282978c51dfcb7ab2fdfed1dbae577652da4ff21
natnaelyimer2040 <106149662+natnaelyimer2040@users.noreply.github.com>
2022-06-21 22:13:44 +0300

    Create Selection_Sort.py

---
+++ Selection Sort/Selection_Sort.py
@@ -0,0 +1,10 @@
+class Solution: 
+ 
+    
+    def selectionSort(self, arr,n):
+        for i in range(1,len(arr)):
+            j=i
+            while arr[j-1]>arr[j] and j>0:
+                arr[j-1],arr[j]=arr[j],arr[j-1]
+                j-=1
+        return(arr)
